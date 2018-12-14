a1819c29e92fa92569e68b8854ff46de61a78ad7
AnaSanchezJi <anpasanchezji@ittepic.edu.mx>
2018-12-14 02:42:06 -0700

    Metodos detalles

--- AlumnosCarreraApi/Controllers/AlumnoCarreraController.cs
+++ AlumnosCarreraApi/Controllers/AlumnoCarreraController.cs
@@ -62,32 +62,211 @@ namespace AlumnosCarreraApi.Controllers
                           ac.FechaReg,
                           ac.UsuarioReg,
                           ac.FechaUltMod,
-                          ac.UsuarioMod                          
+                          ac.UsuarioMod
                       };
             string result = JsonConvert.SerializeObject(res);
             return Content(result, "application/json");
         }
 
         [HttpGet]
-        [Route("GetCarrera")]
-        public ContentResult GetCarrera(Int16 id)
+        [Route("GetDetallesAlumnoCarrera/{id}")]
+        public ContentResult GetDetallesAlumnoCarrera(int id)
         {
-            var res = from ac in LoDBContext.eva_cat_carreras
+            var res = (from eac in LoDBContext.eva_alumnos_carreras
+                       where eac.IdAlumno.Equals(id)
+                       join rcp in LoDBContext.rh_cat_personas on eac.IdAlumno equals rcp.IdPersona
+                       join ecc in LoDBContext.eva_cat_carreras on eac.IdCarrera equals ecc.IdCarrera
+                       join ecr in LoDBContext.eva_cat_reticulas on eac.IdReticula equals ecr.IdReticula
+                       join ece in LoDBContext.eva_cat_especialidades on eac.IdEspecialidad equals ece.IdEspecialidad
+
+                       select new
+                       {
+                           eac.IdAlumno,
+                           rcp.Nombre,
+                           rcp.ApPaterno,
+                           rcp.ApMaterno,
+                           ecc.DesCarrera,
+                           ecr.DesReticula,
+                           ece.DesEspecialidad,
+                           eac.FechaIngreso,
+                           eac.FechaEgreso,
+                           eac.FechaTitulacion,
+                           eac.FechaUltModSII,
+                           eac.PromedioActual,
+                           eac.PromedioPeriodoAnt,
+                           eac.PromedioFinal,
+                           eac.CreditosAprobados,
+                           eac.CreditosCursados,
+                           eac.TotalPuntosVigentes,
+                           eac.TotalPuntosGenerados,
+                           eac.SemestreActual,
+                           eac.Activo,
+                           eac.Borrado,
+                           eac.FechaReg,
+                           eac.UsuarioReg,
+                           eac.FechaUltMod,
+                           eac.UsuarioMod
+                       }).First();
+
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+
+        [Route("ListaAlumnoCarrera")]
+        [HttpGet]
+        public ContentResult GetAlumnoCarreraList()
+        {
+            var res = from eac in LoDBContext.eva_alumnos_carreras
+                      join rcp in LoDBContext.rh_cat_personas on eac.IdAlumno equals rcp.IdPersona
+                      join ecc in LoDBContext.eva_cat_carreras on eac.IdCarrera equals ecc.IdCarrera
                       select new
                       {
+                          IdAlumno = eac.IdAlumno,
+                          Nombre = rcp.Nombre,
+                          ApPaterno = rcp.ApPaterno,
+                          ApMaterno = rcp.ApMaterno,
+                          DesCarrera = ecc.DesCarrera,
+                          FechaIngreso = eac.FechaIngreso,
+                          FechaEgreso = eac.FechaEgreso,
+                          FechaTitulacion = eac.FechaTitulacion,
+                          PromedioActual = eac.PromedioActual,
+                          CreditosAprobados = eac.CreditosAprobados,
+                          SemestreActual = eac.SemestreActual
+                      };
+
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+
+        [HttpGet]
+        [Route("GetCarrera")]
+        public ContentResult GetCarrera2()
+        {
+            var res = from ac in LoDBContext.eva_cat_carreras
+                      
+                      select new
+                      {                         
                           ac.IdCarrera,
-                          ac.DesCarrera,
-                          ac.Alias,
-                          ac.ClaveCarrera,
-                          ac.ClaveOficial                                                    
+                          ac.DesCarrera                         
                       };
             string result = JsonConvert.SerializeObject(res);
             return Content(result, "application/json");
         }
 
+        [HttpGet]
+        [Route("GetEspecialidad")]
+        public ContentResult GetEspecialidad()
+        {
+            var res = from ac in LoDBContext.eva_cat_especialidades
+                      select new
+                      {
+                          ac.IdEspecialidad,
+                          ac.DesEspecialidad
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
 
+        [HttpGet]
+        [Route("GetReticula")]
+        public ContentResult GetReticula()
+        {
+            var res = from ac in LoDBContext.eva_cat_reticulas
+                      select new
+                      {
+                          ac.IdReticula,
+                          ac.DesReticula
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
 
+        [HttpGet]
+        [Route("GetAlumno")]
+        public ContentResult GetAlumno()
+        {
+            var res = from ac in LoDBContext.rh_cat_personas
+                      select new
+                      {
+                          ac.IdPersona,
+                          ac.Nombre
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
 
+        [HttpGet]
+        [Route("GetPeriodo")]
+        public ContentResult GetPeriodo()
+        {
+            var res = from ac in LoDBContext.cat_periodos
+                      select new
+                      {
+                          ac.IdPeriodo,
+                          ac.DesPeriodo
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+
+        [HttpGet]
+        [Route("GetGenerales17")]
+        public ContentResult GetGenerales17()
+        {
+            var res = from ac in LoDBContext.cat_generales
+                      where ac.IdTipoGeneral == 17
+                      select new
+                      {
+                          ac.IdGeneral,
+                          ac.DesGeneral
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+
+        [HttpGet]
+        [Route("GetGenerales25")]
+        public ContentResult GetGenerales25()
+        {
+            var res = from ac in LoDBContext.cat_generales
+                      where ac.IdTipoGeneral == 25
+                      select new
+                      {
+                          ac.IdGeneral,
+                          ac.DesGeneral
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+
+        [HttpGet]
+        [Route("GetGenerales27")]
+        public ContentResult GetGenerales27()
+        {
+            var res = from ac in LoDBContext.cat_generales
+                      where ac.IdTipoGeneral == 27
+                      select new
+                      {
+                          ac.IdGeneral,
+                          ac.DesGeneral
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }
+        [HttpGet]
+        [Route("GetGenerales28")]
+        public ContentResult GetGenerales28()
+        {
+            var res = from ac in LoDBContext.cat_generales
+                      where ac.IdTipoGeneral == 28
+                      select new
+                      {
+                          ac.IdGeneral,
+                          ac.DesGeneral
+                      };
+            string result = JsonConvert.SerializeObject(res);
+            return Content(result, "application/json");
+        }      
         // POST
         [HttpPost]
         [Route("AddAlumnoCarrera")]
@@ -157,7 +336,7 @@ namespace AlumnosCarreraApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<string> Get(Int16 id)
+        public ActionResult<string> GetCarrera(Int16 id)
         {
             var edificio = LoDBContext.eva_alumnos_carreras.Find(id);
             if (edificio == null) { return new ObjectResult("No encontrado"); }
